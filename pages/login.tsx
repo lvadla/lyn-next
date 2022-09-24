50f41e4733461458578eb60d6ab59522b0100bae
Lance Vadla <lance.vadla@pexip.com>
2022-09-24 23:26:25 +0200

    feat: form validation with @mantine/form

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,28 +2,39 @@ import React, { useState, useRef } from "react";
 import { useRouter } from "next/router";
 import {
   Autocomplete,
+  AutocompleteProps,
   Button,
   Container,
+  Group,
   Loader,
   Paper,
   PasswordInput as MantinePasswordInput,
 } from "@mantine/core";
+import { useForm } from "@mantine/form";
 import { useLogin } from "../hooks/useLogin";
 import { setJwtToken } from "../hooks/useJwtToken";
+
+const initialFormValues = {
+  email: "",
+  password: "",
+};
 
 const Login = () => {
   const router = useRouter();
   const login = useLogin();
 
-  const handleSubmit = async (event: React.SyntheticEvent) => {
-    event.preventDefault();
-    const target = event.target as typeof event.target & {
-      email: { value: string };
-      password: { value: string };
-    };
+  const form = useForm({
+    initialValues: initialFormValues,
+
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+    },
+  });
+
+  const handleSubmit = async (values: typeof initialFormValues) => {
     const data = {
-      email: target.email.value,
-      password: target.password.value,
+      email: values.email,
+      password: values.password,
     };
     const { token } = await login(data);
     //FIXME: better. sessionStorage is probably fleeting. testing needed
@@ -34,18 +45,21 @@ const Login = () => {
   return (
     <Container size={420} my={40}>
       <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-        <form onSubmit={handleSubmit}>
-          <AutocompleteLoading />
+        <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
+          <AutocompleteLoading {...form.getInputProps("email")} />
           <MantinePasswordInput
             label="Password"
             required={true}
             mt="md"
             placeholder="Your password"
             id="password"
+            {...form.getInputProps("password")}
           />
-          <Button fullWidth mt="xl" type="submit">
-            Sign in
-          </Button>
+          <Group position="right">
+            <Button mt="xl" type="submit">
+              Sign in
+            </Button>
+          </Group>
         </form>
       </Paper>
     </Container>
@@ -54,7 +68,7 @@ const Login = () => {
 
 export default Login;
 
-export function AutocompleteLoading() {
+export function AutocompleteLoading(props: AutocompleteProps) {
   const timeoutRef = useRef<number>(-1);
   const [value, setValue] = useState("");
   const [loading, setLoading] = useState(false);
@@ -81,13 +95,14 @@ export function AutocompleteLoading() {
   };
   return (
     <Autocomplete
+      {...props}
       required={true}
       value={value}
-      data={data}
       onChange={handleChange}
       rightSection={loading ? <Loader size={16} /> : null}
       label="Email"
       placeholder="Your email address"
+      data={data}
     />
   );
 }
