678f8f458adaa6e42861b9c0f74692a137a9ebab
Lance Vadla <lance.vadla@pexip.com>
2022-09-25 21:55:12 +0200

    feat: bring in visx for temperature graph

--- components/WeatherGraph.tsx
+++ components/WeatherGraph.tsx
@@ -1,11 +1,18 @@
+import React, { useMemo } from "react";
+import { Bar } from "@visx/shape";
+import { Group } from "@visx/group";
+import { LinearGradient } from "@visx/gradient";
+import { scaleBand, scaleLinear } from "@visx/scale";
+import { ParentSize } from "@visx/responsive";
 import {
   AspectRatio,
   Button,
   createStyles,
-  Grid,
   Skeleton,
+  useMantineTheme,
 } from "@mantine/core";
 import { useState } from "react";
+import useGetTemperatureData from "../hooks/useGetTemperatureData";
 
 const useStyles = createStyles((theme) => ({
   controls: {
@@ -41,31 +48,14 @@ function WeatherGraph() {
 
   return (
     <>
-      <AspectRatio ratio={21 / 9} mx="auto">
-        <Skeleton visible={loading} radius="lg">
-          Our business productizes action points to conservatively and globally
-          incentivize our cloud native team player. Our business offshores
-          architectures to dynamically and virtually right-size our mobile
-          stakeholder. We use our self-driving NFTs to iteratively manage our
-          market focus expectations. You need to strategically transform your
-          step-changes to increase your emerging market velocity. So we can hit
-          the ground running, we will be intelligently monetizing every stack in
-          our space. In the visibility space, industry is effectively
-          calibrating its mission critical ballpark figures. In the future, will
-          you be able to reliably innovate capabilities in your business?
-          Efficiencies will come from ethically connecting our deliverables. Our
-          business productizes action points to conservatively and globally
-          incentivize our cloud native team player. Our business offshores
-          architectures to dynamically and virtually right-size our mobile
-          stakeholder. We use our self-driving NFTs to iteratively manage our
-          market focus expectations. You need to strategically transform your
-          step-changes to increase your emerging market velocity. So we can hit
-          the ground running, we will be intelligently monetizing every stack in
-          our space. In the visibility space, industry is effectively
-          calibrating its mission critical ballpark figures. In the future, will
-          you be able to reliably innovate capabilities in your business?
-          Efficiencies will come from ethically connecting our deliverables.
-        </Skeleton>
+      <AspectRatio ratio={21 / 9} mx="auto" sx={{ maxWidth: "960px" }}>
+        {loading ? (
+          <Skeleton radius="lg" />
+        ) : (
+          <ParentSize>
+            {({ width, height }) => <Graph width={width} height={height} />}
+          </ParentSize>
+        )}
       </AspectRatio>
 
       <div className={classes.controls}>
@@ -85,3 +75,92 @@ function WeatherGraph() {
 }
 
 export default WeatherGraph;
+
+const verticalMargin = 80;
+
+interface Weather {
+  minTemperature: number;
+  maxTemperature: number;
+  entries: {
+    time: string;
+    temperature: number;
+    type: string;
+  };
+}
+
+const getTime = (d: Weather["entries"]) => new Date(d.time).getHours();
+const getTemperature = (d: Weather["entries"]) => Number(d.temperature) * 100;
+
+type BarsProps = {
+  width: number;
+  height: number;
+  events?: boolean;
+};
+
+function Graph({ width, height, events = false }: BarsProps) {
+  const {
+    data: {
+      me: {
+        home: { weather },
+      },
+    },
+  } = useGetTemperatureData();
+
+  // bounds
+  const xMax = width;
+  const yMax = height - verticalMargin;
+
+  // scales, memoize for performance
+  const xScale = useMemo(
+    () =>
+      scaleBand<string>({
+        range: [0, xMax],
+        round: true,
+        domain: weather.entries.map(getTime),
+        padding: 0.4,
+      }),
+    [weather.entries, xMax]
+  );
+  const yScale = useMemo(
+    () =>
+      scaleLinear<number>({
+        range: [yMax, 0],
+        round: true,
+        domain: [0, Math.max(...weather.entries.map(getTemperature))],
+      }),
+    [weather.entries, yMax]
+  );
+
+  const theme = useMantineTheme();
+
+  return (
+    <svg width={width} height={height}>
+      <LinearGradient from="#000022" to="#000022" id="dark-blue" />
+      <rect width={width} height={height} fill="url(#dark-blue)" rx={14} />
+      <Group top={verticalMargin / 2}>
+        {weather.entries.map((d: Weather["entries"]) => {
+          const time = getTime(d);
+          const barWidth = xScale.bandwidth();
+          const barHeight = yMax - (yScale(getTemperature(d)) ?? 0);
+          const barX = xScale(time.toString());
+          const barY = yMax - barHeight;
+          return (
+            <Bar
+              key={`bar-${time}`}
+              x={barX}
+              y={barY}
+              width={barWidth}
+              height={barHeight}
+              textDecoration={JSON.stringify(d)}
+              fill={theme.colors.gray[2]}
+              onClick={() => {
+                if (events)
+                  alert(`clicked: ${JSON.stringify(Object.values(d))}`);
+              }}
+            />
+          );
+        })}
+      </Group>
+    </svg>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,13 @@
 import type { NextPage } from "next";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import useGetTemperatureData from "../hooks/useGetTemperatureData";
 import SupportingGrid from "../components/SupportingGrid";
 import HeroText from "../components/HeroText";
 import WeatherGraph from "../components/WeatherGraph";
 
 const Home: NextPage = () => {
-  // const { data } = useGetTemperatureData();
   return (
     <>
-      {/* <p>{JSON.stringify(data)}</p> */}
       <WeatherGraph />
       <HeroText />
       <SupportingGrid />
